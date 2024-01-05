87b693b10ca00c452b3305037feb518650afd1a9
CHISOM EDOKA <edokachisom48@gmail.com>
2024-01-05 01:04:52 +0100

    added student table to database

--- sequelize_wittcode/app.js
+++ sequelize_wittcode/app.js
@@ -1,17 +1,38 @@
 const express = require("express");
-const { sequelize } = require("./models/index");
+const { sequelize, User, Student } = require("./models/index");
 
 const app = express();
+
+// For POST request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(8000, () => {
   console.log("Listening on port 8000");
 });
 
 sequelize
-  .sync()
+  .sync({ alter: true })
   .then(() => {
     console.log("connected to database succesfully");
+  })
+  .then(() => {
+    // const user = User.build({
+    //   username: "chizzy",
+    //   password: "1234",
+    //   age: 23,
+    // });
+    // user.save();
+    // console.log(user.username);
+    const user = {
+      username: "Nonso",
+      password: "123456",
+      age: 19,
+    };
+    User.create(user);
   })
   .catch((err) => {
     console.error(err);
   });
+
+// user.save();
--- sequelize_wittcode/models/index.js
+++ sequelize_wittcode/models/index.js
@@ -9,27 +9,65 @@ const sequelize = new Sequelize({
   host: dbConfig.HOST,
 });
 
-const User = sequelize.define("user", {
-  userId: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const User = sequelize.define(
+  "user",
+  {
+    userId: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: [5, 10],
+      },
+    },
+    age: {
+      type: Sequelize.INTEGER,
+      defaultValue: 18,
+    },
   },
-  username: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
+  { freezeTableName: true, timestamps: false }
+);
+
+const Student = sequelize.define(
+  "student",
+  {
+    student_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: [4, 20],
+      },
+    },
+    favorite_class: {
+      type: Sequelize.STRING,
+      defaultValue: "Computer Science",
+    },
+    school_year: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    subscribed_to_wittcode: {
+      type: Sequelize.BOOLEAN,
+    },
   },
-  password: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  age: {
-    type: Sequelize.INTEGER,
-    defaultValue: 18,
-  },
-});
+  { timestamps: false }
+);
 
 module.exports = {
   sequelize,
   User,
+  Student,
 };
